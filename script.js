3dd6d809146317ba30550ec3cbd22e0f367b5b43
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-13 13:58:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+ let cols = document.querySelectorAll(".image");
+      cols.forEach((col) => {
+        col.addEventListener("dragstart", (e) => {
+          col.style.opacity = 0.5;
+          e.dataTransfer.setData("text", col.id);
+        });
+        col.addEventListener("dragend", (e) => {
+          col.style.opacity = 1;
+        });
+      });
+      let parent = document.getElementById("parent");
+      parent.addEventListener("dragover", (e) => {
+        e.preventDefault();
+      });
+      parent.addEventListener("drop", (e) => {
+        e.preventDefault();
+        let cardId = e.dataTransfer.getData("text");
+        let movingCard = document.getElementById(cardId);
+        parent.appendChild(movingCard);
+      });
